24e8d7ca8560ff649e2a021465a4c8e5e8a7fe60
Jefferson Lisboa de Souza <lisboa.jeff@gmail.com>
2024-04-10 21:48:55 -0300

    env.sh

---
+++ src/env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Primeiro argumento: Caminho para o arquivo .env
+ENV_FILE=$1
+
+if [ ! -f "$ENV_FILE" ]; then
+    echo "Arquivo .env não encontrado: $ENV_FILE"
+    exit 1
+fi
+
+while IFS= read -r line || [[ -n "$line" ]]; do
+    # Ignora linhas vazias e comentários
+    if [[ $line = \#* ]] || [ -z "$lin
