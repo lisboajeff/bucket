40de909699d4e6c08b6467114dc906cf1c5db7fd
Jefferson Lisboa de Souza <lisboa.jeff@gmail.com>
2024-04-13 02:21:36 -0300

    Refactor main function in main.py
    The main function in main.py has been significantly refactored. The argument parser function is removed, and the main function is simplified to accept environment variables from OS. The overall code is made cleaner and easier to read.

--- .github/src/main/main.py
+++ .github/src/main/main.py
@@ -1,6 +1,4 @@
-import argparse
 import os
-from argparse import Namespace, ArgumentParser
 
 import boto3
 
@@ -9,39 +7,19 @@ from file import Device
 from s3 import S3
 
 
-def parse_args():
-    parser: ArgumentParser = argparse.ArgumentParser(description="Synchronizes .pem certificates with an S3 bucket.")
-    parser.add_argument('country', type=str, help='Country')
-    parser.add_argument('environment', type=str, help='Environment')
-    return parser.parse_args()
-
-
 def main():
-    """
-    Main Method Documentation
+    aws_region = os.getenv('AWS_REGION')
 
-    This method is the main entry point for the application. It performs the following tasks:
+    bucket_name = os.getenv('BUCKET_NAME')
 
-      1. Parses the input arguments.
-      2. Initializes a dictionary to track "Uploaded" and "Removed" actions.
-      3. Creates an S3 object with a specified S3 client, bucket name, and actions dictionary.
-      4. Creates a Device object with the actions dictionary and a specified path.
-      5. Creates a Certificate object with the S3 object and the Device object.
-      6. Calls the `upload_certificates` method of the Certificate object, passing the TLS and truststore folder paths.
-      7. Calls the `write_summary_to_file` method of the Device object, specifying the file name for the summary.
-
-    Returns:
-        None
-    """
-    input_args: Namespace = parse_args()
+    path_to_certificate: str = os.getenv('PATH_TO_CERTIFICATE')
 
     actions: dict[str, list[str]] = {"Uploaded": [], "Removed": []}
 
-    s3: S3 = S3(s3_client=boto3.client('s3', region_name=os.getenv('AWS_REGION')), bucket_name=os.getenv('BUCKET_NAME'),
+    s3: S3 = S3(s3_client=boto3.client('s3', region_name=aws_region), bucket_name=bucket_name,
                 actions=actions)
 
-    device: Device = Device(actions=actions,
-                            path=os.path.join("env", input_args.country, input_args.environment, "certificates"))
+    device: Device = Device(actions=actions, path=path_to_certificate)
 
     certificate: Certificate = Certificate(s3=s3, device=device)
 
