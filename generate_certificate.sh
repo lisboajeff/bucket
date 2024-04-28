42cc65aef7104defef529d9c2e930b0f90733165
Jefferson Lisboa de Souza <lisboa.jeff@gmail.com>
2024-04-27 23:34:16 -0300

    Update certificate and certificate generation script
    The posto.pem certificate was updated in the Brasil/dev/certificates location. The certificate generation script now expects a fourth argument for a backup directory and perform backup for the certificates and keys before deleting the private key.

--- generate_certificate.sh
+++ generate_certificate.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 # Validate input parameters
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <Common Name> <Destination Directory> <Extension>"
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <Common Name> <Destination Directory> <Extension> <Backup>"
     exit 1
 fi
 
 COMMON_NAME=$1
 DESTINATION_DIR=$2
 EXTENSION=$3
+BACKUP=$4
 
 # Make the destination directory if it doesn't exist
 mkdir -p "${DESTINATION_DIR}"
+mkdir -p "${BACKUP}"
 
 # Define the path to the key and certificate files
 KEY_FILE_PATH="${DESTINATION_DIR}/${COMMON_NAME}.key"
@@ -22,6 +24,10 @@ openssl genrsa -out "${KEY_FILE_PATH}" 2048
 
 # Create a self-signed x509 certificate
 openssl req -new -x509 -key "${KEY_FILE_PATH}" -out "${CERT_FILE_PATH}" -days 365 -subj "/CN=${COMMON_NAME}"
+
+# Backup
+cp "${CERT_FILE_PATH}" "${BACKUP}/"
+cp "${KEY_FILE_PATH}" "${BACKUP}/"
 
 # Delete the private key
 rm "${KEY_FILE_PATH}"
